3a5e394f6227cb853f85273c88bd80aecac9acd5
harryszh <harrys.zh12@gmail.com>
2024-09-11 15:58:49 +0200

    Header

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+      <div>
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/dashboard">Dashboard</RouterLink>
+          <RouterLink to="/login">Login</RouterLink>
+        </nav>
+      </div>
+    </header>
+  </template>
+  
+  <script setup>
+  import { RouterLink } from "vue-router";
+  </script>
