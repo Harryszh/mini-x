d6cb4f47cb614bc8ce61f7b24bd1a6fc98f0929b
harryszh <harrys.zh12@gmail.com>
2024-09-11 15:09:21 +0200

    middeware

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+Route::group(['middleware' => ['auth:sanctum']], function (){
+    Route::get('/user', [UserController::class,'show']);
+});
+
+
 Route::get('/posts', [PostController::class, 'index']);
 Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/{id}', [PostController::class, 'show']);
